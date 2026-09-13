e1d47da80f59aff1486836cdd057a744cd902956
Tamago0926 <torigoe-ch@outlook.jp>
2026-09-13 22:34:25 +0900

    Build static site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // GitHub Pagesで画像の最適化機能（next/image）が使えないため無効化
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
